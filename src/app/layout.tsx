01d2ce6eeb6d7ce7d1d5b3a9b90f08381a1c7276
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-10-08 13:25:13 +0900

    add vercel Speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -12,5 +13,10 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return children;
+  return (
+    <>
+      {children}
+      <SpeedInsights />
+    </>
+  );
 }
