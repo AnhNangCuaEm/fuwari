78b669d8cdefd2051bf1fe0f850797cad422b9fc
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-10-08 10:08:53 +0900

    add env for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,6 +13,7 @@ module.exports = {
       PORT: 3001,
       // PM2 will also load .env file automatically
     },
+    env_file: '.env',
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
